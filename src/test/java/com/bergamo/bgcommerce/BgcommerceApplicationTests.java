a784d35a8030e65bc226f6ebdf455963efdee1fa
Alisson Bergamin <alisson.bergs@gmail.com>
2025-05-26 20:56:47 -0300

    Project create

---
+++ src/test/java/com/bergamo/bgcommerce/BgcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bergamo.bgcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
