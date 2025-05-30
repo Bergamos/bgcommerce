34df5491994234d078252d0aa3494c26f20c308c
Alisson Bergamin <alisson.bergs@gmail.com>
2025-05-30 06:01:03 -0300

    POST new product

--- src/main/java/com/bergamo/bgcommerce/service/ProductServices.java
+++ src/main/java/com/bergamo/bgcommerce/service/ProductServices.java
@@ -19,14 +19,24 @@ public class ProductServices {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
-
     }
 
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
         return result.map(ProductDTO::new);
+    }
 
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
     }
 
 
